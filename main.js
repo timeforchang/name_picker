76c1a27a9d3a17ec92bdb83a276f993b9914163c
chang.andrew.23@gmail.com <andrew_chang@lawn-143-215-59-115.lawn.gatech.edu>
2017-11-04 00:48:00 -0400

    sign in

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 var getBtn = document.getElementById("getBtn");
 var listID = document.getElementById("databaseID");
+var provider = new firebase.auth.GoogleAuthProvider();
 
 function getName() {
     var id = listID.value;
@@ -25,4 +26,24 @@ function makeList() {
     var id = listID.value;
     var firebaseRef = firebase.database().ref();
     firebaseRef.child("New Branch").set("Hello");
+}
+
+function signIn() {
+    firebase.auth().signInWithPopup(provider).then(function(result) {
+        // This gives you a Google Access Token. You can use it to access the Google API.
+        var token = result.credential.accessToken;
+        // The signed-in user info.
+        var user = result.user;
+        // ...
+    }).catch(function(error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        // The email of the user's account used.
+        var email = error.email;
+        // The firebase.auth.AuthCredential type that was used.
+        var credential = error.credential;
+        // ...
+    });
+    window.location.href = '/index';
 }
