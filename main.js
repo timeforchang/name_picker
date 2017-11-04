7798455711e29b7f8582131894cd492700355833
chang.andrew.23@gmail.com <andrew_chang@lawn-143-215-59-115.lawn.gatech.edu>
2017-11-04 03:36:11 -0400

    making sure it doesn't break

--- main.js
+++ main.js
@@ -1,22 +1,36 @@
-var getBtn = document.getElementById("getBtn");
 var listID = document.getElementById("databaseID");
-var provider = new firebase.auth.GoogleAuthProvider();
+var userName = document.getElementById("name");
 
 function getName() {
     var id = listID.value;
+    var returnName = "";
+    var foundKey = 0;
     var root = firebase.database().ref(id);
-    root.on('value', function(snap) {
+    var uName = userName.value;
+    root.once('value', function(snap) {
         var vals = snap.val();
         var keys = Object.keys(vals);
-        console.log(vals);
-        var returnName = "";
         var rand = Math.floor(Math.random() * keys.length);
+        console.log(vals);
         for (var i = 0; i < keys.length; i++) {
             if (i == rand) {
                 var k = keys[i];
+                foundKey = k;
                 var name = vals[k];
-                vals[k].remove();
-                returnName = name;
+                // console.log(name);
+                // console.log(uName);
+                if (name === uName) {
+                    k = keys[Math.floor(Math.random() * keys.length)];
+                    foundKey = k;
+                    name = vals[k];
+                    returnName = name;
+                    // console.log(returnName);
+                    root.child(foundKey).remove();
+                } else {
+                    returnName = name;
+                    // console.log(returnName);
+                    root.child(foundKey).remove();
+                }
             }
         }
         document.getElementById("returnedName").innerHTML = returnName;
@@ -24,40 +38,36 @@ function getName() {
 }
 
 function makeList() {
-    var id = listID.value;
+    //repopulate
+    var names = ['Justin', 'Andrew', 'Allen', 'Frank', 'Yu Hong', 'Shannon', 'Vivian'];
     var firebaseRef = firebase.database().ref();
-    firebaseRef.child("New Branch").set("Hello");
+    var newID = firebaseRef.push();
+    document.getElementById("listID").innerHTML = "Your new list's ID is: " + newID.key;
+    for (var i = 0; i < names.length; i++) {
+        firebaseRef.child(newID.key).push(names[i]);
+    }
+    console.log(newID.key);
 }
 
 function signIn() {
-    firebase.auth().signInWithPopup(provider).then(function(result) {
-        // This gives you a Google Access Token. You can use it to access the Google API.
-        var token = result.credential.accessToken;
-        // The signed-in user info.
-        var user = result.user;
-        // ...
-        document.getElementById("mainDiv").style.display = "block";
-        document.getElementById("signOut").style.display = "block";
-        document.getElementById("signIn").style.display = "none";
-    }).catch(function(error) {
-        // Handle Errors here.
-        var errorCode = error.code;
-        var errorMessage = error.message;
-        // The email of the user's account used.
-        var email = error.email;
-        // The firebase.auth.AuthCredential type that was used.
-        var credential = error.credential;
-        // ...
+    firebase.auth().signInAnonymously();
+    firebase.auth().onAuthStateChanged(firebaseUser => {
+        console.log(firebaseUser);
     });
+    console.log("logged in");
+    document.getElementById("mainDiv").style.display = "block";
+    document.getElementById("signOut").style.display = "block";
+    document.getElementById("addDiv").style.display = "block";
+    document.getElementById("signIn").style.display = "none";
+    document.getElementById("welcome").style.display = "none";
 }
 
 function signOut() {
-    firebase.auth().signOut().then(function() {
-        // Sign-out successful.
-        document.getElementById("mainDiv").style.display = "none";
-        document.getElementById("signOut").style.display = "none";
-        document.getElementById("signIn").style.display = "block";
-    }).catch(function(error) {
-        // An error happened.
-    });
+    firebase.auth().signOut();
+    // Sign-out successful.
+    document.getElementById("mainDiv").style.display = "none";
+    document.getElementById("signOut").style.display = "none";
+    document.getElementById("addDiv").style.display = "none";
+    document.getElementById("signIn").style.display = "block";
+    document.getElementById("welcome").style.display = "block";
 }
